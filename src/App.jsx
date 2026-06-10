313b3c9f6135fd60b795a5c232e2b6f26f21f5c3
Rajendra Kurup <rajendrakurupkk59@gmail.com>
2026-06-10 15:36:38 +0530

    Session 12 - React props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-function App() {
+import { useState } from "react";
+function ServiceCard(props) {
   return (
     <div>
-      <h1>Welcome to Nexus360!</h1>
-      <p>This is my first React component.</p>
+      <h2>{props.name}</h2>
+      <p>Price: ₹{props.price}</p>
     </div>
   );
 }
-
+function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+    <div>
+      <h2>Count: {count}</h2>
+        <button onClick={() => setCount(count + 1)}>
+          Add One
+      </button>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>Nexus360 Services</h1>
+      <ServiceCard name="Web Design" price="10000" />
+      <ServiceCard name="SEO" price="5000" />
+      <ServiceCard name="Branding" price="8000" />
+       <Counter />
+    </div>
+    
+  );
+}
 export default App;
